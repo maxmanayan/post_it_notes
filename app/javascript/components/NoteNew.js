cdb1fe4877cbc77fb345a4070744d06f51ea11a2
Max Manayan <maximillian521@gmail.com>
2021-02-23 19:45:35 -0700

    createNoteForm css added

--- app/javascript/components/NoteNew.js
+++ app/javascript/components/NoteNew.js
@@ -1,45 +1,67 @@
 import React from "react";
 
+// const NoteNew = (props) => {
+//   return(
+//     <div>
+//       <h1>Create a New Note</h1>
+      
+//       <form action='/notes/' method='post'>
+//         <p>Title</p>
+//         <input name='note[title]' />
+
+//         <p>Description</p>
+//         <textarea name='note[description]' />
+
+//         <hr/>
+//         <button type='submit'>Add</button>
+//         <a href='/notes/'>Cancel</a>
+//       </form>
+//     </div>
+//   )
+// };
+
 const NoteNew = (props) => {
   return(
-    <div>
-      <h1>Create a New Note</h1>
-      
-      <form action='/notes/' method='post'>
-        <p>Title</p>
-        <input name='note[title]' />
 
-        <p>Description</p>
-        <textarea name='note[description]' />
+    <div> 
+      <body className="main_background">
+        <header>
+          <h1>Create a New Note</h1>
+          {/* <a href='/notes/new'>Create New Note</a> */}
+        </header>
+        
+        <div className="notes_background">
 
-        <hr/>
-        <button type='submit'>Add</button>
-        <a href='/notes/'>Cancel</a>
-      </form>
-    </div>
+          <form action='/notes/' method='post'>
+            <div className="post_it_note">
+            
+              <div className="post_it_note_content">
+              
+                <p>Title</p>
+                <input name='note[title]' />
+
+                <p>Description</p>
+                <textarea name='note[description]' />
+              
+              </div>
+          
+          
+
+              <div className="post_it_note_options">
+                <button type='submit'>Add</button>
+                <a href='/notes/'>Cancel</a>
+              </div>
+           
+            </div>
+          </form>
+
+        </div>
+      </body>
+    </div>  
   )
-};
-
+}
 
 export default NoteNew;
 
 
-const NoteNew = (props) => {
-  return(
-    <div>
-      <h1>Create a New Note</h1>
-      
-      <form action='/notes/' method='post'>
-        <p>Title</p>
-        <input name='note[title]' />
 
-        <p>Description</p>
-        <textarea name='note[description]' />
-
-        <hr/>
-        <button type='submit'>Add</button>
-        <a href='/notes/'>Cancel</a>
-      </form>
-    </div>
-  )
-};
