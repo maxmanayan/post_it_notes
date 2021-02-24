227f211c16149774e6d7e54b82c2861ca36b4244
Max Manayan <maximillian521@gmail.com>
2021-02-23 19:11:33 -0700

    editNoteForm css complete

--- app/javascript/components/NoteEdit.js
+++ app/javascript/components/NoteEdit.js
@@ -1,25 +1,67 @@
 import React from "react";
 
+// const NoteEdit = (props) => {
+//   return(
+//     <div>
+//       <h1>Edit Note</h1>
+
+//       <form action={`/notes/${props.note.id}`} method='post'>
+//         <input type='hidden' name="_method" value="patch" />
+//         <p>Title</p>
+//         <input defaultValue={props.note.title} type='text' name='note[title]' />
+
+//         <p>Description</p>
+//         <textarea defaultValue={props.note.description} type='text' name='note[description]' />
+
+//         <hr/>
+//         <button type='submit'>Submit</button>
+//         <a href={/notes/}>Cancel</a>
+//       </form>
+//     </div>
+//   )
+// }
+
 const NoteEdit = (props) => {
   return(
-    <div>
-      <h1>Edit Note</h1>
 
-      <form action={`/notes/${props.note.id}`} method='post'>
-        <input type='hidden' name="_method" value="patch" />
-        <p>Title</p>
-        <input defaultValue={props.note.title} type='text' name='note[title]' />
+    <div> 
+      <body className="main_background">
+        <header>
+          <h1>Edit Note</h1>
+          {/* <a href='/notes/new'>Create New Note</a> */}
+        </header>
+        
+        <div className="notes_background">
 
-        <p>Description</p>
-        <textarea defaultValue={props.note.description} type='text' name='note[description]' />
+          <form action={`/notes/${props.note.id}`} method='post'>
+            <div className="post_it_note">
+            
+              <div className="post_it_note_content">
+              
+                <input type='hidden' name="_method" value="patch" />
+                <p>Title</p>
+                <input defaultValue={props.note.title} type='text' name='note[title]' />
 
-        <hr/>
-        <button type='submit'>Submit</button>
-        <a href={/notes/}>Cancel</a>
-      </form>
-    </div>
+                <p>Description</p>
+                <textarea defaultValue={props.note.description} type='text' name='note[description]' />
+              
+              </div>
+          
+          
+
+              <div className="post_it_note_options">
+                <button type='submit'>Submit</button>
+                <a href={/notes/}>Cancel</a>
+              </div>
+           
+            </div>
+          </form>
+
+        </div>
+      </body>
+    </div>  
   )
 }
 
-
 export default NoteEdit;
+
--- app/javascript/components/NoteNew.js
+++ app/javascript/components/NoteNew.js
@@ -22,3 +22,24 @@ const NoteNew = (props) => {
 
 
 export default NoteNew;
+
+
+const NoteNew = (props) => {
+  return(
+    <div>
+      <h1>Create a New Note</h1>
+      
+      <form action='/notes/' method='post'>
+        <p>Title</p>
+        <input name='note[title]' />
+
+        <p>Description</p>
+        <textarea name='note[description]' />
+
+        <hr/>
+        <button type='submit'>Add</button>
+        <a href='/notes/'>Cancel</a>
+      </form>
+    </div>
+  )
+};
